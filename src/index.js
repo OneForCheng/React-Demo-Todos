41daf2fc417d015281f4ef8f57451762ecabd3eb
Cheng Huang <chuang@CNchuang.local>
2018-09-09 10:00:25 +0800

    [Forcheng] feat: use localStorage to persist store state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  Provider
-} from 'react-redux'
-import {
-  createStore
-} from 'redux'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import reducers from './reducers'
 import App from './App';
+import { loadState, saveState } from './connectivity/localStorage'
 
+const persistedState = loadState();
 const store = createStore(
   reducers,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 ReactDOM.render( <Provider store = {
     store
