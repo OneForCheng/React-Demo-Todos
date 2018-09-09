d46208bc6c987d91c8ba336c4eed6f16b2b959da
Cheng Huang <chuang@CNchuang.local>
2018-09-09 15:34:08 +0800

    [Forcheng] test: add two tests of ClearBtn component

--- test/components/foot.spec.js
+++ test/components/foot.spec.js
@@ -1,10 +1,12 @@
 import { expect } from "chai";
 import React from "react";
 import { shallow } from "enzyme";
+import sinon from 'sinon';
 import { Footer, Filters, ClearBtn } from "../../src/components/footer";
 import "../testSetup";
 
 describe("test Footer component", function() {
+
   it("should correctly render Footer component", function() {
     const props = {
       filters: {},
@@ -16,4 +18,32 @@ describe("test Footer component", function() {
     expect(component.find("ClearBtn").exists()).to.eql(true);
     expect(component.find("Filters").exists()).to.eql(true);
   });
+
+});
+
+
+describe("test ClearBtn component", function() {
+
+  it("should be empty render when isShowClearBtn given false", function() {
+    const props = {
+      isShowClearBtn: false,
+      clearCompleted: () =>{}
+    };
+    const component = shallow(<ClearBtn {...props} />);
+    expect(component.isEmptyRender()).to.eql(true);
+  });
+
+  it("should invoke clearCompleted when click the clearCompleted button", function() {
+    const clickSpy = sinon.spy();
+    const props = {
+      isShowClearBtn: true,
+      clearCompleted: clickSpy
+    };
+    
+    const component = shallow(<ClearBtn {...props} />);
+    component.find('button').simulate('click');
+    expect(clickSpy.calledOnce).to.eql(true);
+
+  });
+
 });
