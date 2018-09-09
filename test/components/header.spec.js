1fede27d123392749f24f700dbca4ea469aa5474
Cheng Huang <chuang@CNchuang.local>
2018-09-09 18:23:19 +0800

    [Forcheng] fix: replace 'keyDown' with 'keyPress' in the test description

--- test/components/header.spec.js
+++ test/components/header.spec.js
@@ -6,7 +6,7 @@ import { Header } from "../../src/components/header";
 import "../testSetup";
 
 describe("test Header component", function() {
-  it("should not invoke addTodo when keyDown enter on the input box and the input value is empty", function() {
+  it("should not invoke addTodo when keyPress enter on the input box and the input value is empty", function() {
     const spy = sinon.spy();
     const props = {
       addTodo: spy
@@ -18,7 +18,7 @@ describe("test Header component", function() {
     expect(spy.calledOnce).to.eql(false);
   });
 
-  it("should invoke addTodo when keyDown enter on the input box and the input value is not empty", function() {
+  it("should invoke addTodo when keyPress enter on the input box and the input value is not empty", function() {
     const spy = sinon.spy();
     const props = {
       addTodo: spy
