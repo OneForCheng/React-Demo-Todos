7151a54c26dbc023caa39e80ddbc4f6fe9f7de8e
Cheng Huang <chuang@CNchuang.local>
2018-09-09 15:49:15 +0800

    [Forcheng] test: add two tests for the Filters component

--- test/components/foot.spec.js
+++ test/components/foot.spec.js
@@ -1,12 +1,11 @@
 import { expect } from "chai";
 import React from "react";
 import { shallow } from "enzyme";
-import sinon from 'sinon';
+import sinon from "sinon";
 import { Footer, Filters, ClearBtn } from "../../src/components/footer";
 import "../testSetup";
 
 describe("test Footer component", function() {
-
   it("should correctly render Footer component", function() {
     const props = {
       filters: {},
@@ -18,16 +17,13 @@ describe("test Footer component", function() {
     expect(component.find("ClearBtn").exists()).to.eql(true);
     expect(component.find("Filters").exists()).to.eql(true);
   });
-
 });
 
-
 describe("test ClearBtn component", function() {
-
   it("should be empty render when isShowClearBtn given false", function() {
     const props = {
       isShowClearBtn: false,
-      clearCompleted: () =>{}
+      clearCompleted: () => {}
     };
     const component = shallow(<ClearBtn {...props} />);
     expect(component.isEmptyRender()).to.eql(true);
@@ -39,11 +35,62 @@ describe("test ClearBtn component", function() {
       isShowClearBtn: true,
       clearCompleted: clickSpy
     };
-    
+
     const component = shallow(<ClearBtn {...props} />);
-    component.find('button').simulate('click');
+    component.find("button").simulate("click");
     expect(clickSpy.calledOnce).to.eql(true);
+  });
+});
+
+describe("test Filters component", function() {
+  it("should correctly render Filters component", function() {
+    const props = {
+      filters: [
+        {
+          label: "All",
+          selected: true
+        },
+        {
+          label: "Completed",
+          selected: false
+        },
+        {
+          label: "Active",
+          selected: false
+        }
+      ],
+      switchFilter: ()=>{}
+    };
+
+    const component = shallow(<Filters {...props} />);
+    expect(component.find("li").length).to.eql(3);
 
   });
 
+  it("should invoke all switchFilter when selected label is all", function() {
+    const clickSpy = sinon.spy();
+    const props = {
+      filters: [
+        {
+          label: "All",
+          selected: true
+        },
+        {
+          label: "Completed",
+          selected: false
+        },
+        {
+          label: "Active",
+          selected: false
+        }
+      ],
+      switchFilter: clickSpy
+    };
+
+    const component = shallow(<Filters {...props} />);
+    
+    component.find("a.selected").simulate('click');
+    expect(clickSpy.calledWith("All")).to.eql(true);
+
+  });
 });
