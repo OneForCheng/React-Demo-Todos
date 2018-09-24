d6b912e44e72fe45171d4ef785184d3812548b46
Cheng Huang <chuang@CNchuang.local>
2018-09-24 15:07:59 +0800

    [Forcheng] feat: add flow config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-    root: true,    
-    parserOptions: {
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-            modules: true
-        }
-    },
-    env: {
-        browser: true,
-    },
-    rules: {
-        // "indent": ["error", 2]
-    },
-    plugins:["react"]
-}
+  root: true,
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    }
+  },
+  env: {
+    browser: true
+  },
+  extends: ["plugin:flowtype/recommended"],
+  parser: "babel-eslint",
+  rules: {
+    // "indent": ["error", 2]
+  },
+  plugins: ["flowtype", "react"]
+};
